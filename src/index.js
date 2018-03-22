46306c2d43d9d58ecfbaf4998080aeb8e09071ca
Oliver Fencott <oliverfencott@gmail.com>
2018-03-22 00:33:24 +0000

    Add config and build scripts

--- src/index.js
+++ src/index.js
@@ -1,95 +1,47 @@
-const {
-  // Array
-  reduce,
-  reduceRight,
-  map,
-  forEach,
-  every,
-  filter,
-  some,
-  includes,
-  find,
-  findIndex,
-  join,
-  lastIndexOf,
-  reverse,
-  pop,
-  push,
-  shift,
-  sort,
-
-  // String
-  split,
-  match,
-  repeat,
-  search,
-  toLowerCase,
-  toUpperCase,
-  trim,
-  trimLeft,
-  trimRight,
-  padEnd,
-  padStart,
-
-  // String/Array
-  concat,
-  replace,
-  indexOf,
-  slice,
-
-  // Number
-  toFixed,
-
-  // Utils
-  curry,
-  pipe,
-  compose
-} = require('./utils');
-
 module.exports = {
   // Array
-  reduce,
-  reduceRight,
-  map,
-  forEach,
-  every,
-  filter,
-  some,
-  includes,
-  find,
-  findIndex,
-  join,
-  lastIndexOf,
-  reverse,
-  pop,
-  push,
-  shift,
-  sort,
+  reduce: require('./reduce'),
+  reduceRight: require('./reduceRight'),
+  map: require('./map'),
+  forEach: require('./forEach'),
+  every: require('./every'),
+  filter: require('./filter'),
+  some: require('./some'),
+  includes: require('./includes'),
+  find: require('./find'),
+  findIndex: require('./findIndex'),
+  join: require('./join'),
+  lastIndexOf: require('./lastIndexOf'),
+  reverse: require('./reverse'),
+  pop: require('./pop'),
+  push: require('./push'),
+  shift: require('./shift'),
+  sort: require('./sort'),
 
   // String
-  split,
-  match,
-  repeat,
-  search,
-  toLowerCase,
-  toUpperCase,
-  trim,
-  trimLeft,
-  trimRight,
-  padEnd,
-  padStart,
+  split: require('./split'),
+  match: require('./match'),
+  repeat: require('./repeat'),
+  search: require('./search'),
+  toLowerCase: require('./toLowerCase'),
+  toUpperCase: require('./toUpperCase'),
+  trim: require('./trim'),
+  trimLeft: require('./trimLeft'),
+  trimRight: require('./trimRight'),
+  padEnd: require('./padEnd'),
+  padStart: require('./padStart'),
 
   // String/Array
-  concat,
-  replace,
-  indexOf,
-  slice,
+  concat: require('./concat'),
+  replace: require('./replace'),
+  indexOf: require('./indexOf'),
+  slice: require('./slice'),
 
   // Number
-  toFixed,
+  toFixed: require('./toFixed'),
 
   // Utils
-  curry,
-  pipe,
-  compose
+  curry: require('./curry'),
+  pipe: require('./pipe'),
+  compose: require('./compose'),
 };
